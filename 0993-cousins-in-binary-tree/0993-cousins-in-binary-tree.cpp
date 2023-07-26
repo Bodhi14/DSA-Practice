dbf5026604c3eaf9570bdf77003e685f0e827b5d
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2023-07-26 12:38:37 +0530

    Time: 7 ms (18.62%), Space: 11.5 MB (8.21%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -11,42 +11,43 @@
  */
 class Solution {
 public:
-void dfs(TreeNode* root,int prevNodeValue, int currentDepth, int x, int y, int& depthX, int& parentX, int& depthY, int& parentY) {
+    unordered_map<int, int> parent;
     
-    if(!root)
-        return;
-
-    
-    if(root->val == x) {
-        depthX = currentDepth; parentX = prevNodeValue;
+    bool isSameDepth(TreeNode* root, int node1, int node2) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int n = q.size();
+            bool found1 = false, found2 = false;
+            for(int i=0;i<n;++i) {
+                TreeNode* fr = q.front();
+                q.pop();
+                if(fr->left) {
+                q.push(fr->left);
+                if(fr->left->val==node1) found1 = true;
+                else if(fr->left->val==node2) found2 = true;
+                }
+                if(fr->right) 
+                {
+                q.push(fr->right);
+                if(fr->right->val==node1) found1 = true;
+                else if(fr->right->val==node2) found2 = true;
+                }
+                if(found1 && found2) return true;
+            }
+        }
+        return false;
+    }
+    void dfs(TreeNode* root, int par) {
+        if(!root) return;
+        parent[root->val] = par;
+        dfs(root->left, root->val);
+        dfs(root->right, root->val);
         return;
     }
-
-    
-    if(root->val == y) {
-        depthY = currentDepth; parentY = prevNodeValue;
-        return;
+    bool isCousins(TreeNode* root, int x, int y) {
+         dfs(root, 0);
+         cout<<isSameDepth(root, x, y);
+         return (parent[x]!=parent[y] && isSameDepth(root, x, y));
     }
-
-    dfs(root->left, root->val, currentDepth+1, x, y, depthX, parentX, depthY, parentY);
-    dfs(root->right, root->val, currentDepth+1, x, y, depthX, parentX, depthY, parentY);
-}
-
-
-bool isCousins(TreeNode* root, int x, int y) {
-   
-    if(!root)
-        return false;
-
-    int depthX, parentX; 
-    int depthY, parentY; 
-
-    dfs(root, 0, 0, x, y, depthX, parentX, depthY, parentY);
-
-    
-    if(depthX == depthY && parentX != parentY)
-        return true;
-    else
-        return false;
-}
 };
