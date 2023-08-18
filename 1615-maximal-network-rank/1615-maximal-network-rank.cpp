f53a0c51e1ac08ce40e4d035c6737a925600fdd3
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2023-08-18 17:48:24 +0530

    Time: 146 ms (16.85%), Space: 36.9 MB (40.52%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        //corner cases
+        
+        if(!roads.size()) return {};
+        
+        //end of corner cases
+        int ans = 0;
+        unordered_map<int, int> mp;
+        set<pair<int, int>> s;
+        vector<pair<int, int>> degrees;
+        for(vector<int> v: roads) {
+            s.insert({v[0], v[1]});
+            mp[v[0]]++;
+            mp[v[1]]++;
+        }
+        for(auto it:mp) {
+            degrees.push_back({it.first, it.second}); 
+        }
+        sort(degrees.begin(), degrees.end());
+        for(auto pp:degrees) cout<<pp.first<<" "<<pp.second<<"\n";
+        int m = degrees.size();
+        for(int i=0;i<m;++i) {
+            int connection1 = degrees[i].second;
+            for(int j=i+1;j<m;++j) {
+                if(j==i) continue;
+                int connection2 = degrees[j].second;
+                int connections = connection1 + connection2;
+                if(s.find({degrees[i].first, degrees[j].first})!=s.end() || s.find({degrees[j].first, degrees[i].first})!=s.end()) {
+                    cout<<degrees[i].first<<" and "<<degrees[j].first<<" are connected"<<"\n";
+                    --connections;
+                }
+                // cout<<degrees[i].second<<" "<<degrees[j].second<<" "<<connections<<"\n";
+                ans = max(ans, connections);
+            }
+        }
+        return ans;
+        
+        
+    }
+};
