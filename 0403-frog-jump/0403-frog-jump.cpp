0bd8d53e84259999299e4b3fb28d4112ef7b435f
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2023-08-27 09:19:40 +0530

    Time: 313 ms (25.20%), Space: 232.6 MB (18.98%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -4,16 +4,17 @@ public:
         //base case
         if(ind==ss-1) return true;
         if(ind>=ss) return false;
+        //memoization
         if(dp[ind][last_jump]!=-1) return dp[ind][last_jump];
         bool jump = false;
         for(int j=ind+1;j<ss;++j) {
+            //dp states
             if((stones[j]-stones[ind])>=last_jump-1 && (stones[j]-stones[ind])<=last_jump+1) jump = jump || solve(stones, ss, j, stones[j]-stones[ind], dp);
         }
         return dp[ind][last_jump] = jump;
     }
     bool canCross(vector<int>& stones) {
         int n = (int)stones.size();
-        int max_diff = stones[n-1] - stones[0];
         vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
         return solve(stones, n, 0, 0, dp);
     }
