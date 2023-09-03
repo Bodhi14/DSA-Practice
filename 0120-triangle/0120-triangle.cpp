1a9b09abacaf807741f9c68836c455e3d992f067
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2023-09-03 23:12:40 +0530

    Time: 2 ms (94.24%), Space: 9.1 MB (7.47%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,13 +4,15 @@ private:
         int n = triangle.size();
     
         
-        if(i>=n) return 0;
+        if(i>=n) return 0; // I am returning a zero sum
         
         //memoization
         if(dp[i][j]!=-1) return dp[i][j];
         
-        int st1 = solve(triangle, i+1, j, dp);
-        int st2 = solve(triangle, i+1, j+1, dp);
+        //I can initialize the states with the undesired values/ floor condition values
+        int st1 = 1e4 + 1, st2 = 1e4 + 1;
+        if(i<=n-1) st1 = solve(triangle, i+1, j, dp);
+        if(i<=n-1) st2 = solve(triangle, i+1, j+1, dp);
         return dp[i][j] = triangle[i][j] + min(st1, st2);
     }
 public:
