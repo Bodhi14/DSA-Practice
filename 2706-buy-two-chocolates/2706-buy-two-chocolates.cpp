f478ac84e461230fab817bbd9df58bf66d61f56d
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-10 20:38:37 +0530

    Time: 12 ms (60.20%), Space: 46.9 MB (96.71%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-         int n = (int)prices.size();
-         sort(prices.begin(), prices.end());
-         int AmountLeft;
-         for(int i=1;i<n;++i) {
-             AmountLeft = money-prices[i]-prices[i-1];
-             if(AmountLeft>=0) return AmountLeft;
-         }
-         return money;
+        int n = (int)prices.size();
+        if(n<=1) return money;
+        int ans = INT_MAX;
+        sort(prices.begin(), prices.end());
+        for(int i = 0; i<n-1; ++i) {
+            for(int j = i+1; j<n; ++j) {
+                if(prices[i]+prices[j] > money) continue;
+                else ans = min(ans, (prices[i]+prices[j]));
+            }
+        }
+        return ans==INT_MAX ? money : money-ans;
     }
 };
