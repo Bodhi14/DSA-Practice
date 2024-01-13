83e2072aa190385cf717f4b556ebbe527caf175f
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-13 09:10:09 +0530

    Create file

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,16 @@
+        j=0;
+        i=0;
+        while(j<n){
+            if(nums[j]!=0){
+                nums[i]=nums[j];
+                i++;
+            }
+            j++;
+        }
+        while(i<n){
+            nums[i]=0;
+            i++;
+        }
+    }
+};
+[
