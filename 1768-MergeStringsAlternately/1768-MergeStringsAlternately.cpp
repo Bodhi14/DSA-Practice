579d1c99b918d6d5e99b165fa3d77b7ad68ecd00
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-18 23:58:50 +0530

    Create file

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.cpp
@@ -0,0 +1,18 @@
+        while(i<min(n, m)) {
+            ans += word1[i];
+        }
+            ans += word2[i];
+        if(n<m) {
+            ans += word2.substr(i);
+        }
+            i++;
+        else ans += word1.substr(i);
+        int i = 0;
+        int m = word2.size();
+        int n = word1.size();
+        string ans;
+    string mergeAlternately(string word1, string word2) {
+        return ans;
+    }
+};
+"
