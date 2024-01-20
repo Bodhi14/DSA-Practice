1c48ca224fcff45d54e88c9151a028781743a12a
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-20 23:23:22 +0530

    Create file

---
+++ 443-StringCompression/443-StringCompression.cpp
@@ -0,0 +1,10 @@
+        for(int i = 1;i<n;++i) {
+            if(chars[i]==chars[i-1]) count++;
+        int n = (int)chars.size();
+
+        int count = 1;
+        string temp;
+    int compress(vector<char>& chars) {
+            else {
+                temp+=chars[i-1];
+[
