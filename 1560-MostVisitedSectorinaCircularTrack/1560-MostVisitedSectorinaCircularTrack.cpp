19b7f8a2e1c17b0c27e617c0a7e9388a030e7b90
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-27 23:20:01 +0530

    Create file

---
+++ 1560-MostVisitedSectorinaCircularTrack/1560-MostVisitedSectorinaCircularTrack.cpp
@@ -0,0 +1,11 @@
+                ans.push_back(i);
+            }
+            for(int i = rounds[0]; i<=rounds[m-1]; ++i) {
+        if(rounds[0] <= rounds[m-1]) {
+
+        }
+        else {
+            for(int i = 1;i<=rounds[m-1];++i) ans.push_back(i);
+            for(int i = rounds[0]; i<=n; ++i) ans.push_back(i);
+        }
+4
