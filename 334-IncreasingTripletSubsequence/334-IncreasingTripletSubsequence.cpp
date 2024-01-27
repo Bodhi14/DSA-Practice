815ff948ab36b0a12f1d702e93592bd9f968a445
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-27 23:22:05 +0530

    Update file 1/27/2024, 11:22:04 PM

--- 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.cpp
+++ 334-IncreasingTripletSubsequence/334-IncreasingTripletSubsequence.cpp
@@ -1,11 +1,11 @@
-        for(int i=1;i<n;++i) {
-            minNum[i] = min(minNum[i-1], nums[i]);
-        }
         for(int i=n-2;i>-1;i--) maxNum[i] = max(maxNum[i+1], nums[i]);
-        vector<int> minNum(n, INT_MAX), maxNum(n, INT_MIN);
-    bool increasingTriplet(vector<int>& nums) {
-        int n = (int)nums.size();
-        if(n<=2) return false;
-        minNum[0] = nums[0];
+        for(int i=1;i<n-1;++i) {
+            if(nums[i]>minNum[i-1] && nums[i]<maxNum[i+1]) return true;
+        }
+        return false;
+    }
+        }
+            minNum[i] = min(minNum[i-1], nums[i]);
+        for(int i=1;i<n;++i) {
         maxNum[n-1] = nums[n-1];
-[
+[1,2,3,4,5]
