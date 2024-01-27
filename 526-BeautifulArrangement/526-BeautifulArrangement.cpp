98d588acf52cac22d213018c8753ac07449b55f7
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-27 17:55:13 +0530

    Create file

---
+++ 526-BeautifulArrangement/526-BeautifulArrangement.cpp
@@ -0,0 +1,10 @@
+        }
+
+        if(cur_num >= n+1){
+            ans++;
+            return;
+        //base case
+class Solution {
+public:
+    void solve(vector<int>& nums, int n, int& ans, int cur_num){
+2
