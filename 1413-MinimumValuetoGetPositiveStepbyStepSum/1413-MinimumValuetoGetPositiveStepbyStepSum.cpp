bbe496822a91b88cceb298717a2c5a279d2443c8
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-29 00:32:57 +0530

    Create file

---
+++ 1413-MinimumValuetoGetPositiveStepbyStepSum/1413-MinimumValuetoGetPositiveStepbyStepSum.cpp
@@ -0,0 +1,11 @@
+        while(low<=high) {
+            int mid = high - (high-low)/2;
+            if(isValid(nums, mid)) {
+        int low = 1, high = max_num;
+        max_num++;
+        max_num *= n;
+        }
+            max_num = max(max_num, abs(x));
+        for(int x:nums) {
+        int max_num = INT_MIN;
+[
