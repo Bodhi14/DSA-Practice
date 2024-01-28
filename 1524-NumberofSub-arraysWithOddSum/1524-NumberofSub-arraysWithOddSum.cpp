fbad0693c2fe48a1c830ff5499fe1ce77a03e658
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-28 19:03:25 +0530

    Update file 1/28/2024, 7:03:24 PM

--- 1524-NumberofSub-arraysWithOddSum/1524-NumberofSub-arraysWithOddSum.cpp
+++ 1524-NumberofSub-arraysWithOddSum/1524-NumberofSub-arraysWithOddSum.cpp
@@ -1,10 +1,10 @@
-        }
-            odd[0] = 1;
-        else even[0] = 1;
-        for(int i = 1; i<n; ++i) {
-            ans += 1;
-            int curr = arr[i];
-            if(arr[i]%2) {
-                odd[i] = even[i-1] + 1;// +1 for the odd number separately being a 
 subarray and even[i-1] for the curr number taken with the prev odd sum subarray
+                CurrOdd = PrevEven + 1;// +1 for the odd number separately being a 
+            if(arr[i]%2) {
+        for(int i = 0; i<n; ++i) {
+    int numOfSubarrays(vector<int>& arr) {
+        int n = (int)arr.size(), ans = 0, CurrOdd = 0, CurrEven = 0, PrevEven = 0, 
+PrevOdd = 0;
+                CurrEven = PrevOdd;
+            }
 [
