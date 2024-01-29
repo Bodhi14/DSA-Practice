c5201e6a422016ac4db72bbd497ab1b5dd033c33
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-29 20:51:30 +0530

    Create file

---
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.cpp
@@ -0,0 +1,11 @@
+            if(isVowel(curr)) currSum++;
+        }
+        maxSum = currSum;
+        for(int i = 1; (i+k-1)<n; ++i) {
+            char curr = s[i];
+        for(int i = 0; i<k; ++i) {
+        int currSum = 0, maxSum;
+        int n = (int)s.size();
+            if(isVowel(s[i-1])) currSum--;
+            if(isVowel(s[i+k-1])) currSum++;
+"
