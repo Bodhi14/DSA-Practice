91ffb977f0aab8b552dce9e11e0a696aa6673aff
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-01-29 18:38:24 +0530

    Create file

---
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.cpp
@@ -0,0 +1,11 @@
+            }
+                st1.pop();
+                st2.push(st1.top());
+        else {
+            while(!st1.empty()) {
+    }
+    
+    void push(int x) {
+        if(st1.empty()) st1.push(x);
+
+[
