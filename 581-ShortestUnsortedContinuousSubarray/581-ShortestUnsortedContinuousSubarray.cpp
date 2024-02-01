36dedbfbf652d7b9d04ebbbdc841724b2ceab09c
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-02 00:39:55 +0530

    Create file

---
+++ 581-ShortestUnsortedContinuousSubarray/581-ShortestUnsortedContinuousSubarray.cpp
@@ -0,0 +1,10 @@
+            }
+                end = i; 
+            if(max > nums[i]){ 
+        int max = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+    int findUnsortedSubarray(vector<int>& nums) {
+        int end = -1;
+class Solution {
+public:
+[
