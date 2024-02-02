ea15dce7d86686e8eb844647cabda4d17c505992
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-02 21:40:43 +0530

    Create file

---
+++ 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.cpp
@@ -0,0 +1,10 @@
+                mp[v[start++]] -= 1;
+            while(mp[v[end - 1]] > k) {
+        while(end < n) {
+            mp[v[end++]] += 1;
+        unordered_map<int, int> mp;
+    int maxSubarrayLength(vector<int>& v, int k) {
+        int n = v.size(), cnt = 0, ans = 0, start = 0, end = 0;
+public:
+class Solution {
+[1,2,3,1,2,3,1,2]
