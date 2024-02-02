f3d17ba4d1656dc15a5e3f6d93de5d560ce93a00
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-02 22:01:57 +0530

    Update file 2/2/2024, 10:01:55 PM

--- 2733-NeitherMinimumnorMaximum/2733-NeitherMinimumnorMaximum.cpp
+++ 2733-NeitherMinimumnorMaximum/2733-NeitherMinimumnorMaximum.cpp
@@ -2,9 +2,9 @@
             min_num = min(min_num, x);
             max_num = max(max_num, x);
         }
-        for(int x:nums) if(x!=min_num && x!=max_num) return x;
-        return -1;
         int min_num = INT_MAX, max_num = INT_MIN;
     int findNonMinOrMax(vector<int>& nums) {
 public:
+class Solution {
+        if(nums.size()<=2) return -1;
 [
