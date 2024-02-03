c0b755f86f1589ba83ac4a0e0e66d0097dbefe12
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-03 13:57:14 +0530

    Update file 2/3/2024, 1:57:13 PM

--- 39-CombinationSum/39-CombinationSum.cpp
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -1,11 +1,9 @@
-        temp.push_back(candidates[j]);
-        sum+=candidates[j];
-        for(int j=i;j<n;++j) {
-        solve(candidates, n, target, j, sum, temp, ans);
-        sum-=candidates[j];
-        temp.pop_back();
-        }
-        return;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(candidates, n, target, 0, 0, temp, ans);
+        return ans;
     }
-        }
+};
 [
