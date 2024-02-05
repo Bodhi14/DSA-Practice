49f24ac328073f232fcadcf21044b727a5169a85
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-05 19:31:48 +0530

    Create file

---
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -0,0 +1,10 @@
+        unsigned int mid=(st+et)/2;
+        unsigned int et=n;
+        unsigned int st=1;
+    int firstBadVersion(int n) {
+public:
+class Solution {
+
+// bool isBadVersion(int version);
+// The API isBadVersion is defined for you.
+5
