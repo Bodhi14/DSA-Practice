c1de0e02c8f29db4f194444a4efbd67b20cf0242
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-08 20:29:05 +0530

    Update file 2/8/2024, 8:29:05 PM

--- 1338-ReduceArraySizetoTheHalf/1338-ReduceArraySizetoTheHalf.cpp
+++ 1338-ReduceArraySizetoTheHalf/1338-ReduceArraySizetoTheHalf.cpp
@@ -1,10 +1,11 @@
                 cnt++;
             }
         }
-        return cnt;
-    }
                 newSize -= x;
             if(newSize>n/2){
         for(int x:v) {
-};
+        int m = v.size();
+        sort(v.begin(), v.end(), greater<int>());
+        for(auto &it:mp) v.push_back(it.second);
+            if(newSize<n/2) return cnt;
 [
