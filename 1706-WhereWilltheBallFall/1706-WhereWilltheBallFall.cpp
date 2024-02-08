72b666a96e436bd1735926676dd246e38a86a8c6
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-08 18:41:09 +0530

    Create file

---
+++ 1706-WhereWilltheBallFall/1706-WhereWilltheBallFall.cpp
@@ -0,0 +1,24 @@
+        {
+            dfs(i+1,j+1,m,n,grid);
+        }
+        else
+        {
+            dfs(i+1,j-1,m,n,grid);
+        }
+    }
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int m=grid.size();
+        if(grid[i][j]==1)
+        }
+            return;
+            ans.push_back(-1);
+        if((grid[i][j]==1&&grid[i][j+1]==-1)||(grid[i][j]==-1&&grid[i][j-1]==1))
+        {
+        }
+            ans.push_back(-1);
+            return ;
+        {
+        if((grid[i][j]==1&&j==n-1)||(grid[i][j]==-1&&j==0))
+        }
+            }
+[[1,1,1,-1,-1],[1,1,1,-1,-1],[-1,-1,-1,1,1],[1,1,1,1,-1],[-1,-1,-1,-1,-1]]
