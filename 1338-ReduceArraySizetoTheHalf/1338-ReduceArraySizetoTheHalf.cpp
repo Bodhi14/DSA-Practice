1bc6f9cdabce192fce199e7ec6c7bb14458b257b
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-08 20:28:18 +0530

    Update file 2/8/2024, 8:28:18 PM

--- 1338-ReduceArraySizetoTheHalf/1338-ReduceArraySizetoTheHalf.cpp
+++ 1338-ReduceArraySizetoTheHalf/1338-ReduceArraySizetoTheHalf.cpp
@@ -1,11 +1,10 @@
-        sort(v.begin(), v.end(), greater<int>());
-        int m = v.size();
-        for(auto &it:mp) v.push_back(it.second);
-        for(int x:arr) mp[x]++;
-        unordered_map<int, int> mp;
-        vector<int> v;
-        int newSize = n;
-        int cnt = 0, n = (int)arr.size();
-    int minSetSize(vector<int>& arr) {
-public:
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+                newSize -= x;
+            if(newSize>n/2){
+        for(int x:v) {
+};
 [
