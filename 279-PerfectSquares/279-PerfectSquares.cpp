7e9be5dfb324bb5a1b1e0374fe2fbc8f591a3d4a
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-09 11:30:28 +0530

    Create file

---
+++ 279-PerfectSquares/279-PerfectSquares.cpp
@@ -0,0 +1,11 @@
+    }
+        return dp[tgt] = ans;
+        }
+            if(arr[j]<=tgt) ans = min(ans, 1 + solve(j, arr, m, tgt-arr[j], dp));
+        for(int j = i; j<m; ++j) {
+        if(dp[tgt]!=-1) return dp[tgt];
+        int ans = 1e7;
+        if(i>=m || tgt==0) return 0;
+        if(arr[i]>tgt) return 1e7;
+
+1
