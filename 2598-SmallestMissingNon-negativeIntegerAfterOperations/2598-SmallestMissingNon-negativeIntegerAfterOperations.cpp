45c9cf3ec9bea28f768dbe70bd85c9bbe7ddc4f7
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-11 03:15:16 +0530

    Create file

---
+++ 2598-SmallestMissingNon-negativeIntegerAfterOperations/2598-SmallestMissingNon-negativeIntegerAfterOperations.cpp
@@ -0,0 +1,11 @@
+                {
+                if(nums[i]<0)
+                int c=nums[i]%value;
+        {
+        for(int i=0;i<n;i++)
+        map<int,int> mp;
+       
+        vector<int> pre(n,0);
+        int n=nums.size();
+                    int ck=abs(nums[i])/value;
+[
