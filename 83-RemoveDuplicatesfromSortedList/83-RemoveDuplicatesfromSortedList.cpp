65dd07f7558430cb84834b53d48e0e928cc33ae4
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-11 02:10:18 +0530

    Update file 2/11/2024, 2:10:17 AM

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
@@ -1,11 +1,10 @@
-            if(s.find(curr->val)!=s.end()) {
-                prev->next = curr->next;
-                ListNode* todel = curr;
                 curr = curr->next;
                 delete(todel);
-                continue;
-            }
-            else s.insert(curr->val);
-            prev = curr;
-            curr=curr->next;
+                ListNode* todel = curr;
+                prev->next = curr->next;
+            if(prev->val == curr->val) {
+        while(curr) {
+        ListNode* prev = head, *curr = head->next;
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!(head) || !(head->next)) return head;
 [
