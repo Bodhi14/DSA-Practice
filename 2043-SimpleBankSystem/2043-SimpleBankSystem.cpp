1df901023b63cab61214a3b7ced72c49d7903bdc
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-13 02:45:47 +0530

    Create file

---
+++ 2043-SimpleBankSystem/2043-SimpleBankSystem.cpp
@@ -0,0 +1,11 @@
+        account2--;
+    bool transfer(int account1, int account2, long long money) {
+        account1--;
+    
+    }
+        accounts = balance;
+    vector<long long> accounts;
+    Bank(vector<long long>& balance) {
+class Bank {
+public:
+[
