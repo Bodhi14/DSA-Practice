42171922b7fc2c42556d72f2ff7025f71f0655cb
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-13 02:54:53 +0530

    Create file

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+        return nums[midEle];
+        int n = (int)nums.size();
+        if(n==1) return nums[0];
+        int midEle = n/2;
+        if(!(n%2)) midEle--;
+[
