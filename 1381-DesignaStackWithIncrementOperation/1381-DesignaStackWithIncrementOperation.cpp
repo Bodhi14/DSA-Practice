6b0ecdaa583dc837751abbe7756e4c413194b562
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-14 16:31:22 +0530

    Create file

---
+++ 1381-DesignaStackWithIncrementOperation/1381-DesignaStackWithIncrementOperation.cpp
@@ -0,0 +1,11 @@
+        if(stk.size() < maxsize ) stk.push_back(x);
+    void push(int x) {
+    
+    CustomStack(int maxSize) {
+        maxsize = maxSize;
+    }
+    int maxsize;
+    deque<int> stk;
+public:
+class CustomStack {
+[
