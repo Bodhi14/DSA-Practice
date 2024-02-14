5b5162647a5701bc40e5e37145bc128c863b352b
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-15 02:36:40 +0530

    Update file 2/15/2024, 2:36:39 AM

--- 791-CustomSortString/791-CustomSortString.cpp
+++ 791-CustomSortString/791-CustomSortString.cpp
@@ -1,8 +1,12 @@
-    string customSortString(string order, string s) {
-        str = order;
-        sort(s.begin(), s.end(), compare);
-            return s;
-    }
-};
-string Solution::str;
+····string·customSortString(string·order,·string·s)·{
+········str·=·order;
+········sort(s.begin(),·s.end(),·compare);
+············return·s;
+····}
+
+········return·str.find(char1)·<·str.find(char2);
+····static·bool·compare(char·char1,·char·char2){
+····static·string·str;··
+····}
+public:
 "
