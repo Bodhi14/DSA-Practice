dceaf0967274fba04c3db19dde2ec51b8f10be1c
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-14 16:48:27 +0530

    Create file

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -0,0 +1,11 @@
+        for(auto str: strs) {
+            int ss = (int)str.size();
+            if(min_size > ss) {
+                min_size = ss;
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        int min_size = INT_MAX;
+        string min_str, ans;
+class Solution {
+public:
+[
