dc625166f02130e61765e55fe26af05f6ddeed31
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-15 02:11:33 +0530

    Create file

---
+++ 791-CustomSortString/791-CustomSortString.cpp
@@ -0,0 +1,12 @@
+            }
+                }
+                    if(mp[ch]==0) mp.erase(ch);
+                    mp[ch]--;
+                    ans+=ch;
+                while(mp.count(ch)) {
+                vis[ch]++;
+            if(mp.find(ch)!=mp.end()) {
+        for(char& ch: order) {
+        for(char& ch: s) mp[ch]++;
+        }
+"
