238125135a117f58894f812ea9ea644859da32f4
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-15 02:39:27 +0530

    Create file

---
+++ 2236-RootEqualsSumofChildren/2236-RootEqualsSumofChildren.cpp
@@ -0,0 +1,10 @@
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        return root->val == root->left->val + root->right->val;
+    }
+};
+[
