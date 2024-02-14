85d74b6affa5025510a54e29169d862179483f2a
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-14 22:52:22 +0530

    Create file

---
+++ 1380-LuckyNumbersinaMatrix/1380-LuckyNumbersinaMatrix.cpp
@@ -0,0 +1,13 @@
+            for(int j = 0; j<m; ++j) {
+                if(matrix[i][j] == rowMin[i] && matrix[i][j] == colMax[j]) {
+        for(int i = 0; i<n; ++i) {
+        }
+            colMax[i] = max_num;
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+                    break;
+        }
+        return ans;
+    }
+[
