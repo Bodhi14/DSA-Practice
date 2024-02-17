b7115ba8b2c77a53c84d0ef132b59a7993b982fe
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-17 23:56:13 +0530

    Create file

---
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.cpp
@@ -0,0 +1,12 @@
+    {
+        return m[key][high].second;
+    }
+    
+    return "";
+    }
+    private: 
+    unordered_map<string, vector<pair<int,string>>>m;
+};
+
+/**
+["TimeMap","set","get","get","set","get","get"]
