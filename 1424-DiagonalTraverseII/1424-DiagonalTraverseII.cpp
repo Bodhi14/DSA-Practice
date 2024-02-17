c2cdcdfdef1c73a1d766f749b7114f01db861056
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-17 18:22:10 +0530

    Update file 2/17/2024, 6:22:09 PM

--- 1424-DiagonalTraverseII/1424-DiagonalTraverseII.cpp
+++ 1424-DiagonalTraverseII/1424-DiagonalTraverseII.cpp
@@ -2,11 +2,11 @@
         for(int i = 0 ; i<n; ++i) {
                 mp[i+j].push_back(nums[i][j]);
             }
-        map<int, vector<int>> mp;
             int m = (int)nums[i].size();
         }
         for(auto it:mp) {
-            for(int x:it.second) ans.push_back(x);
-            reverse(it.second.begin(), it.second.end());
-            ve
+            for(int x:temp) ans.push_back(x);
+            reverse(temp.begin(), temp.end());
+            vector<int> temp = it.second;
+        }
 [
