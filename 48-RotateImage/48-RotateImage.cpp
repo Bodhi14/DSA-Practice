27d99be1fd6f22fc56e9858c39517fc383786f54
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-17 20:27:45 +0530

    Update file 2/17/2024, 8:27:45 PM

--- 48-RotateImage/48-RotateImage.cpp
+++ 48-RotateImage/48-RotateImage.cpp
@@ -1,13 +1,8 @@
+        for(int i = 0; i<n; ++i) swap(matrix[i].begin(), matrix[i].end());
         }
-        int i = 0, j = n-1;
-        while(i<j) {
-            for(int k = 0; k<n; ++k) {
-                swap(matrix[k][i], matrix[k][j]);
             }
-            i++;
-            j--;
-        }
-    }
+                swap(matrix[i][j], matrix[j][i]);
         return;
+    }
 };
 [
