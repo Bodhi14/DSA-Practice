ab55e6050c34eb1482f0c24f35f6128b2b4ddce3
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-18 20:13:32 +0530

    Create file

---
+++ 1507-ReformatDate/1507-ReformatDate.cpp
@@ -0,0 +1,11 @@
+            ans+='0';
+            ans+=date[0];
+        }
+        else{
+            ans+=date[0];
+            ans+=date[1];
+        }
+        return ans;
+    }
+};
+"20th Oct 2052"
