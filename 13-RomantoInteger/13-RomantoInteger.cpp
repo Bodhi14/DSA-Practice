02a5979ad75888b749d681c6639e39277728d8b9
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-20 18:55:14 +0530

    Create file

---
+++ 13-RomantoInteger/13-RomantoInteger.cpp
@@ -0,0 +1,13 @@
+        int ans=0;
+        mp['M']=1000;
+        mp['C']=100;
+        mp['D']=500;
+        mp['L']=50;
+        mp['X']=10;
+        mp['V']=5;
+        mp['I']=1;
+        map<char, int> mp;
+public:
+    int romanToInt(string s) {
+class Solution {
+"III"
