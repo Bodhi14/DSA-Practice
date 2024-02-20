a5c611ac766744f4d6786ae003cb672f7c9d7dac
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-20 20:15:42 +0530

    Create file

---
+++ 814-BinaryTreePruning/814-BinaryTreePruning.cpp
@@ -0,0 +1,13 @@
+ * };
+ */
+class Solution {
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     int val;
+ *     TreeNode *left;
+ * struct TreeNode {
+/**
+ * Definition for a binary tree node.
+[
