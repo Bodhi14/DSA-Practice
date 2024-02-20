bf4cd8984e52312cd5defd912366100143c043a5
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-20 18:41:33 +0530

    Create file

---
+++ 2810-FaultyKeyboard/2810-FaultyKeyboard.cpp
@@ -0,0 +1,11 @@
+        }
+            }
+        return s;
+               i--;
+               s.erase(s.begin()+i);
+               reverse(s.begin(), s.begin()+i);
+            if(s[i]=='i') {
+        for(int i=0;i<s.size();++i) {
+    }
+};
+"
