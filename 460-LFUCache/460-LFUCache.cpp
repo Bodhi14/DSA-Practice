9012a358b2fe9cc4cb6e85c09fb2a2d55348502f
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-26 21:57:16 +0530

    Create file

---
+++ 460-LFUCache/460-LFUCache.cpp
@@ -0,0 +1,13 @@
+        delNext->prev = delPrev;
+        len--;
+    }
+};
+
+        Node *delPrev = delNode->prev;
+        Node *delNext = delNode->next;
+        delPrev->next = delNext;
+
+    void deleteNode(Node *delNode) {
+        len++;
+    }
+[
