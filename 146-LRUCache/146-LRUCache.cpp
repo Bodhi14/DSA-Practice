40986241a43c0faae1d114e4705e053debeda710
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-26 21:50:31 +0530

    Create file

---
+++ 146-LRUCache/146-LRUCache.cpp
@@ -0,0 +1,12 @@
+        }
+
+        if(m.size() == cap){
+            m.erase(tail -> prev -> key);
+            deleteNode(tail -> prev);
+        }
+
+        addNode(new Node(key, value));
+        m[key] = head -> next;
+    }
+};
+[
