3b6ef16a0115ced707b6a0d88329f4a8e56685f4
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-29 03:42:45 +0530

    Create file

---
+++ 1366-RankTeamsbyVotes/1366-RankTeamsbyVotes.cpp
@@ -0,0 +1,13 @@
+      return a[m]<b[m];
+    }
+    string rankTeams(vector<string>& votes) {
+        i++; 
+      }
+        if(a[i]!=b[i]) return a[i]>b[i];
+      while(i<m){
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+      int m = a.size()-1;
+      int i = 0;
+[
