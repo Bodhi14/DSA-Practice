4acc1bf6f96ed747c850e156fbc9a5f9d4f4449c
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-02-29 19:53:02 +0530

    Create file

---
+++ 1139-Largest1-BorderedSquare/1139-Largest1-BorderedSquare.cpp
@@ -0,0 +1,22 @@
+        for(int j=1;j<n;j++){
+        
+        }
+        for(int i=1;i<m;i++){
+            if(grid[i][0]!=0){
+                dp[i][0]={1+dp[i-1][0].x,1}; 
+            }
+        vector<vector<Co>>dp(m,vector<Co>(n,{0,0}));
+        if(grid[0][0]){
+            dp[0][0]={1,1};  
+        }
+        
+        int m=grid.size(); 
+        int n=grid[0].size();  
+    int largest1BorderedSquare(vector<vector<int>>& grid) {
+class Solution {
+public:
+}; 
+   int y;    
+   int x;
+struct Co{
+[[1,1,1],[1,0,1],[1,1,1]]
