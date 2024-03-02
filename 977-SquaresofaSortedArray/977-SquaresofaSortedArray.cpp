66f95a4e59eb525d3822bbdc0d34ef17bddaace6
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-02 12:20:29 +0530

    Create file

---
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.cpp
@@ -0,0 +1,16 @@
+            int curr;
+            if(abs(nums[low])>abs(nums[high])) {
+                curr = nums[low];
+            }
+                low++;
+        for(int i = 0; i<n; ++i){
+        
+        int low = 0, high =n-1;
+        vector<int> ans(n);
+        if(n==1) return {nums[0]*nums[0]};
+        int n = (int)nums.size();
+            else {
+                curr = nums[high];
+                high--;
+            }
+[
