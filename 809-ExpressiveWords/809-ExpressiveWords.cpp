c250e63e1b6d26eb2659be43821542d8f9071bd6
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-03 01:58:39 +0530

    Create file

---
+++ 809-ExpressiveWords/809-ExpressiveWords.cpp
@@ -0,0 +1,15 @@
+            int k = 0;
+            bool flag = true;
+            while (j < s.length() && flag)
+            {
+    int expressiveWords(string s, vector<string>& words) {
+        int count = 0;
+        for (auto w : words)
+        {
+            if (w.length() > s.length())
+                continue;
+            int i = 0;
+            int j = 0;
+public:
+class Solution {
+"
