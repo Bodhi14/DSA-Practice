c293dfae1ef1f58dad473705a044a04d5d2855e5
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-03 20:31:54 +0530

    Create file

---
+++ 1328-BreakaPalindrome/1328-BreakaPalindrome.cpp
@@ -0,0 +1,15 @@
+        for (int i = 0; i < n/2; i++){
+            if(str[i] != 'a'){
+                str[i] = 'a';
+                return str;
+            }
+        }
+        str[n - 1] = 'b';
+        return str;
+        int n = (int)str.size(), end = n / 2;
+    string breakPalindrome(string str){
+public:
+        if(n==1) return "";
+    }
+};
+"
