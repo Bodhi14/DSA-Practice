5c09dc82e840df19f31cdcb9a1414fde52fd7a6e
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-04 20:41:35 +0530

    Create file

---
+++ 851-LoudandRich/851-LoudandRich.cpp
@@ -0,0 +1,16 @@
+                    mini=quiet[ele];
+                }
+            }
+            
+                    a=ele;
+                if(mini>=quiet[ele]){
+            for(auto ele:ansc[i]){
+            
+            int mini=quiet[i],a=i;
+        for(int i=0;i<n;i++){
+        vector<int>ans(n);
+        
+        }
+            dfs(i,i,edge,vis,ansc);
+            vector<int>vis(n);
+[[1,0],[2,1],[3,1],[3,7],[4,3],[5,3],[6,3]]
