624d7ce7de34e3d647e97c433043f66da71c317c
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-08 12:14:01 +0530

    Create file

---
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.cpp
@@ -0,0 +1,16 @@
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow =head, *fast = head;
+        while(fast && fast->next) {
+            slow = slow->next;
+        }
+    }
+            fast = fast->next->next;
+        return slow;
+};
+[
