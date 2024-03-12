4baeb93a24b95ff2fa7691cb3d1473c5739599a3
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-12 19:08:37 +0530

    Update file 3/12/2024, 7:08:37 PM

--- 791-CustomSortString/791-CustomSortString.cpp
+++ 791-CustomSortString/791-CustomSortString.cpp
@@ -1,12 +1,14 @@
-····string·customSortString(string·order,·string·s)·{
-········str·=·order;
-········sort(s.begin(),·s.end(),·compare);
-············return·s;
-····}
+        return str.find(char1) < str.find(char2);
+    }
 
-········return·str.find(char1)·<·str.find(char2);
-····static·bool·compare(char·char1,·char·char2){
-····static·string·str;··
-····}
+    string customSortString(string order, string s) {
+        str = order;
+        sort(s.begin(), s.end(), compare);
+        return s;
+    }
+};
+string Solution::str;
+    static bool compare(char char1, char char2){
 public:
-"
+    static string str;  
+"cba"
