a6556c2549266c2e152de14d6f2a257909f12a84
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-18 18:40:32 +0530

    Update file 3/18/2024, 6:40:30 PM

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.cpp
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.cpp
@@ -1,4 +1,4 @@
-        int ans = 1, end = points[0][1];
+        int end = points[0][1];
         for(int i = 1; i<n; ++i) {
             if(points[i][0] > end) {
                 end = points[i][1];
@@ -8,7 +8,7 @@
         }
         return ans;
         sort(points.begin(), points.end());
-        int n = (int)points.size();
+        int ans = 1, n = (int)points.size();
     int findMinArrowShots(vector<vector<int>>& points) {
 public:
 class Solution {
