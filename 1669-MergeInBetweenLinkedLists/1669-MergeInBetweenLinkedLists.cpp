7095582bfa26b6563600255252d8ea45ecbcdb2a
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-20 16:14:15 +0530

    Create file

---
+++ 1669-MergeInBetweenLinkedLists/1669-MergeInBetweenLinkedLists.cpp
@@ -0,0 +1,15 @@
+            end = end->next;
+        }
+        while(b>-1) {
+        }
+        ListNode* start = list1, *end = list1;
+        while(a>1) {
+            start = start->next;
+            a--;
+    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+class Solution {
+public:
+        start->next = list2;
+        while(list2->next) {
+            b--;
+[
