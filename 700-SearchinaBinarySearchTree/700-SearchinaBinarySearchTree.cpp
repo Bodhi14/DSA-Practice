5834a10718911451c718f2def2eb00ff662abe42
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-23 14:01:55 +0530

    Create file

---
+++ 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.cpp
@@ -0,0 +1,12 @@
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(!root || root->val == val) return root;
+        if(root->val > val) return searchBST(root->left, val);
+        return searchBST(root->right, val);
+    }
+};
+[
