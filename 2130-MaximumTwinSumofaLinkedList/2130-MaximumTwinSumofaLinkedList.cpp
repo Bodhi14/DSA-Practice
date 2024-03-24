428613f7e0ac540e723cd405e6ff6b262976d11c
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2024-03-24 13:46:17 +0530

    Update file 3/24/2024, 1:46:16 PM

--- 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.cpp
+++ 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.cpp
@@ -1,10 +1,5 @@
     int pairSum(ListNode* head) {
         int ans = INT_MIN;
-public:
-class Solution {
- */
- * };
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
         vector<int> v;
         while(head) {
             v.push_back(head->val);
@@ -13,4 +8,8 @@ class Solution {
         int i = 0, j = (int)(v.size() - 1);
         while(i<j) {
             ans = max(ans, v[i++] + v[j--]);
+        }
+        return ans;
+    }
+};
 [
